4b3c0070eec5c0e612ea8b805e5ba6cc9ce9302c
alex gunkow <gunkow@gmail.com>
2016-03-25 19:51:45 +0200

    prettify

--- app/api/controllers.py
+++ app/api/controllers.py
@@ -3,6 +3,7 @@ from flask import request
 import requests
 from requests.exceptions import ConnectionError
 
+
 class Add(Resource):
     def post(self):
         r = request
@@ -10,15 +11,43 @@ class Add(Resource):
         dub_id = r.form['dub_id']
         emo_id = r.form['emo_id']
 
-        script = "e = g.addEdge('belongs', g.V(%s), g.V(%s)); " % (user_id, dub_id)
-        script += "e.setProperty('emo_id', %s)" % emo_id
-        script = '{"gremlin": "%s"}' % script
+        db_proxy = DbProxy()
+        db_proxy.add("e = g.addEdge('belongs', g.V(%s), g.V(%s)); " % (user_id, dub_id))
+        db_proxy.add("e.setProperty('emo_id', %s)" % emo_id)
         try:
-            response = requests.post('http://ec2-52-50-189-192.eu-west-1.compute.amazonaws.com:8182', data=script)
+            response = db_proxy.send()
         except ConnectionError as e:
             return 500, "db abruption"
         if response.status_code == 500:
             return 400, response.json()['message']
-        return "ok"
+        return response.status_code, "ok"
 
-def 
+
+class Singleton(type):
+    def __init__(cls, name, bases, dict):
+        super(Singleton, cls).__init__(name, bases, dict)
+        cls.instance = None
+
+    def __call__(cls, *args, **kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+        return cls.instance
+
+
+class DbProxy(object):
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        self.script = ""
+
+    def add(self, line):
+        self.script += line
+
+    def send(self):
+        self.script = '{"gremlin": "%s"}' % self.script
+        try:
+            response = requests.post('http://ec2-52-50-189-192.eu-west-1.compute.amazonaws.com:8182', data=self.script)
+        except ConnectionError as e:
+            raise e
+        self.script = ''
+        return response
