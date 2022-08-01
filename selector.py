c2fc70e786005319aae5c6445e31aa9d4defe052
gunkow <alex@carbonre.tech>
2022-08-01 20:32:43 +0300

    fast commit

--- selector.py
+++ selector.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
 
+import sys
 import datetime as dt
-from os.path import dirname
 from pygit2 import Repository
 
 repo = Repository('.git')
 
-dirs = set()
+new_files = sys.argv[1:]
 
-def add_dir(p):
-    if p and p not in dirs:
-        dirs.add(p)
-        add_dir(dirname(p))
-
-for p in repo.diff('HEAD', 'HEAD^'):
-    add_dir(dirname(p.delta.new_file.path))
-    add_dir(dirname(p.delta.old_file.path))
 
 matchers = {
     #"src/ezer": "ezer.test",
    # "src/nada/app": "nada.test",
   #  "src/nada/utils/lambda": "lambda-produce.test",
-    "src/dashboard": "lambda-deploy.test",
+    "app": "lambda-deploy.test",
   #  "src/infra/aws/workload/nada": "infra-nada.test",
   #  "src/infra/aws/management/dns": "dns.test",
   #  "src/infra/aws/management/stacksets": "stacksets.test",
@@ -30,7 +22,12 @@ matchers = {
 
 ts = dt.datetime.now().strftime('%Y-%m-%dT%H%M%S')
 
-for d in dirs:
-    if d in matchers:
-        print(matchers[d])
-        print(f"{matchers[d]}@{ts}")
+tags = set()
+for f in new_files:
+    for m in matchers:
+      if f.startswith(m):
+        tags.add(matchers[m])
+
+tags = map(lambda m: f"{m}@{ts}", matchers)
+
+print(list(tags))
