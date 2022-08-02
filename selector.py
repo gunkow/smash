0555906f2e462f5e464b067357253b6f8bc406cb
gunkow <alex@carbonre.tech>
2022-08-02 17:20:36 +0300

    fast commit

--- selector.py
+++ selector.py
@@ -2,14 +2,15 @@
 
 import sys
 import datetime as dt
-from pathlib import Path
+from github import Github
 
-import pygit2
+github_token = sys.argv[1]
+new_files = sys.argv[2:]
 
-repo = pygit2.Repository('.git')
-
-new_files = sys.argv[1:]
-
+g = Github(github_token)
+for repo in g.get_user().get_repos():
+    print(repo.name)
+    
 matchers = {
     # "src/ezer": "ezer.test",
     # "src/nada/app": "nada.test",
@@ -36,24 +37,27 @@ print(f"tags: {tags}")
 with open('tags.txt', 'w') as f:
     f.write("\n".join(tags))
 
+# import os
+# for tag in tags:
+#     os.system(f'git tag {tag}')
+#     os.system(f'git push origin {tag}')
 import os
-for tag in tags:
-    os.system(f'git tag {tag}')
-    os.system(f'git push origin {tag}')
+
+os.environ.get("")
 
 first_commit = repo.revparse_single("HEAD")
 tagger = pygit2.Signature("github CI", "noreply@carbonre.tech")
-# for tag in tags:
-#     print("(tag, first_commit.oid.hex, pygit2.GIT_OBJ_COMMIT, tagger)",
-#           (tag, first_commit.oid.hex, pygit2.GIT_OBJ_COMMIT, tagger, ""))
-#     print(repo.create_tag(tag, first_commit.oid.hex, pygit2.GIT_OBJ_COMMIT, tagger, ""))
+for tag in tags:
+    print("(tag, first_commit.oid.hex, pygit2.GIT_OBJ_COMMIT, tagger)",
+          (tag, first_commit.oid.hex, pygit2.GIT_OBJ_COMMIT, tagger, ""))
+    print(repo.create_tag(tag, first_commit.oid.hex, pygit2.GIT_OBJ_COMMIT, tagger, ""))
 
-# ori_remote = repo.remotes[0]
-# userName = "gunkow"
+ori_remote = repo.remotes[0]
+userName = "gunkow"
 # credentials = pygit2.UserPass(userName, password)
-# credentials = pygit2.Keypair(userName, "~/.ssh/id_rsa.pub", "~/.ssh/id_rsa", "")
+credentials = pygit2.Keypair(userName, "~/.ssh/id_rsa.pub", "~/.ssh/id_rsa", "")
 # credentials = pygit2.KeypairFromMemory(userName, str(Path('~/.ssh/id_rsa.pub').expanduser().read_text()), str(Path('~/.ssh/id_rsa').expanduser().read_text()), "")
 
-# ori_remote.credentials = credentials
-# callbacks = pygit2.RemoteCallbacks(credentials=credentials)
-# ori_remote.push(["refs/heads/master"], callbacks=callbacks)
+ori_remote.credentials = credentials
+callbacks = pygit2.RemoteCallbacks(credentials=credentials)
+ori_remote.push(["refs/heads/master"], callbacks=callbacks)
